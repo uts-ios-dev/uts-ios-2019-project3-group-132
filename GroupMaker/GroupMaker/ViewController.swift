1cfafdf88b5ed3777aafe12f515f13f36d72f127
Zhe Jiang <jason.bzangel@gmail.com>
2019-05-23 15:59:19 +1000

    Add Xcode Project
    Created a Single-View Xcode Project to start working on the storyboard for wireframe

---
+++ GroupMaker/GroupMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GroupMaker
+//
+//  Created by Jason JIANG on 23/5/19.
+//  Copyright © 2019 Group 132. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
