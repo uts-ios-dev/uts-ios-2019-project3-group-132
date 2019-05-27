67f04bba1a1c82297737e711a18bf4b203727a6c
Callum Crossley <Callum@Davids-MBP.gateway>
2019-05-27 21:41:02 +1000

    Added main view controllers

---
+++ GroupMaker/GroupMaker/Controller/GroupViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GroupViewController.swift
+//  GroupMaker
+//
+//  Created by Callum Crossley on 27/5/19.
+//  Copyright © 2019 Group 132. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
