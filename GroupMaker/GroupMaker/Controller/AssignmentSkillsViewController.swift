e94b9428345ebaa79df7c1a75762211fafe7744d
laramontana <larabrightside@gmail.com>
2019-06-06 15:22:28 +1000

    Added Interests view

---
+++ GroupMaker/GroupMaker/Controller/AssignmentSkillsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AssignmentSkillsViewController.swift
+//  GroupMaker
+//
+//  Created by Lara on 06/06/2019.
+//  Copyright © 2019 Group 132. All rights reserved.
+//
+
+import UIKit
+
+class AssignmentSkillsViewController : UIViewController {
+    
+    @IBAction func backButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ GroupMaker/GroupMaker/Controller/InterestsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InterestsViewController.swift
+//  GroupMaker
+//
+//  Created by Lara on 06/06/2019.
+//  Copyright © 2019 Group 132. All rights reserved.
+//
+
+import UIKit
+
+class InterestsViewController : UIViewController {
+    
+    
+    @IBAction func saveButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
